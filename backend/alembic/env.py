2fd6f85c46592c9466d331e09c7c06bc054c9b19
Muhsin Kalodi <muhsinkalodi9311@gmail.com>
2026-03-13 16:13:07 +0530

    upated_render_elements

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -60,8 +60,11 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
+    from database import SQLALCHEMY_DATABASE_URL
+    configuration = config.get_section(config.config_ini_section, {})
+    configuration["sqlalchemy.url"] = SQLALCHEMY_DATABASE_URL
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        configuration,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
